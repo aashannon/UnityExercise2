f61852480b182092b9ef2c37e6e41b9b1de59f01
Aaron Shannon <ayren.shannon@me.com>
2025-06-08 00:45:56 -0700

    completed exercise 2

---
+++ Exercise2/Assets/Scripts/PrintFavoriteGames.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class PrintFavoriteGames : MonoBehaviour
+{
+    /// <summary>
+    /// This script prints a list of favorite games to the console when the game starts.
+    /// </summary>
+    void Start()
+    {
+        Debug.Log("The Legend of Zelda: Breath of the Wild, Dragon Marked for Death, Super Mario Odyssey.");
+    }
+}
